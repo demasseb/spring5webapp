07c178c1d2f4bc338152d4b945c769ef098842ec
Brian <demasseb@gmail.com>
2019-03-14 13:56:40 -0400

    Added publisher repo and spring controller. updates to entities.

--- spring5webapp/src/main/java/com/spring/spring5webapp/repositories/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.spring.spring5webapp.repositories;
-
-import com.spring.spring5webapp.model.Author;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AuthorRepository extends CrudRepository<Author, Long> {
-}
-
--- spring5webapp/src/main/java/com/spring/spring5webapp/repositories/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.spring.spring5webapp.repositories;
-
-import com.spring.spring5webapp.model.Book;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BookRepository extends CrudRepository<Book, Long> {
-}
